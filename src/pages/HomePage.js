241efc379e4322f5e000408bcd14eef4e1f4af0f
jzillifro <zillifrojacob@gmail.com>
2020-04-30 18:23:29 -0500

    fixed homepage image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import boo_img from '../images/boo.jpg';
+
 class HomePage extends React.Component {
   render() {
     return(
       <div class="page">
         <h1>Hi! I'm Flora</h1>
         <div class = "intro">
-        <img class = "pic" src="boo.jpg">
+        <img class = "pic" src={boo_img} alt="boo">
         </img>
         <p class = "profile">oh say can you see by the dawns early light</p>
         </div>
