24ab133f8e94070560a674591f5967bcf89d14e6
Flora He <hflora68@gmail.com>
2020-05-01 18:07:56 -0500

    fixing pictures page

--- src/pages/PicturesPage.js
+++ src/pages/PicturesPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import boo_img from '../images/boo.jpg';
 
 class PicturesPage extends React.Component {
   render() {
@@ -6,7 +7,7 @@ class PicturesPage extends React.Component {
       <div class = "page">
         <h1>My Pics</h1>
         <div class = "pictureContainer">
-            <img class = "pic" src ="boo.jpg"/>
+            <img class = "pic" src ={boo_img} alt = "pic of me"/>
             <img class = "pic" src ="https://c8.alamy.com/comp/GFECA5/new-york-united-states-27th-july-2016-chef-guy-fieri-presented-designed-GFECA5.jpg"/>
             <img class = "pic" src ="https://c8.alamy.com/comp/GFECA5/new-york-united-states-27th-july-2016-chef-guy-fieri-presented-designed-GFECA5.jpg"/>
             <img class = "pic" src ="https://c8.alamy.com/comp/GFECA5/new-york-united-states-27th-july-2016-chef-guy-fieri-presented-designed-GFECA5.jpg"/>
